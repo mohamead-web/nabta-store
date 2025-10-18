69d52119720f83879d0c6cdda582ae2788448465
mohamead-web <mohamead42000@gmail.com>
2025-10-18 20:03:23 +0300

    supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
-import { type Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL and Anon Key must be provided in environment variables.');
-}
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL as string,
+  import.meta.env.VITE_SUPABASE_ANON_KEY as string
+);
